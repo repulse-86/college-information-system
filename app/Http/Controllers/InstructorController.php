ab70d96f3768562e112a6b87f77a47d1d2950a89
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2025-05-04 07:09:30 +0800

    feat: implement redirecting to instructor form and storing of data

---
+++ app/Http/Controllers/InstructorController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreInstructorRequest;
+use App\Models\Instructor;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class InstructorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        return view('instructor');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreInstructorRequest $request): RedirectResponse
+    {
+        Instructor::create($request->validated());
+
+        return redirect()->route('instructors.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
