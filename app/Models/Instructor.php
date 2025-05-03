cd359b77aa66a21a2883f96b49839a06b27917c5
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2025-05-04 07:08:33 +0800

    feat: create Instructor model and migration

---
+++ app/Models/Instructor.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Instructor extends Model
+{
+    /** @use HasFactory<\Database\Factories\InstructorFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+    ];
+}
