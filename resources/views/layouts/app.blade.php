09ff00ec7d42eb6d31e2712a9af47a7773619a6f
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2025-05-04 07:08:13 +0800

    feat: create main layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+    <body>
+        <x-navbar />
+        <main class="items-center lg:justify-center min-h-screen flex-col bg-gray-100">
+            @yield('content')
+        </main>
+    </body>
+</html>
