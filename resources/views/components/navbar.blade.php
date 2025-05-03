d7c9520e77cbc0ea5ee19c57aadc202fc7f1fabd
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2025-05-04 07:07:25 +0800

    feat: create header component

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,7 @@
+<header class="w-full bg-white">
+    <div class="max-w-screen-xl px-4 py-3 flex justify-between items-center">
+        <nav class="justify-between items-center space-x-4">
+            <a href="{{ route('instructors.index') }}" class="hover:text-gray-300">Instructors</a>
+        </nav>
+    </div>
+</header>
