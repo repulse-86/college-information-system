7945bb73e76fc87d970753cee8798f4d968084ba
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2025-05-04 07:09:41 +0800

    feat: user can view instructors page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\InstructorController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/instructors', InstructorController::class)->only(['index', 'store']);
