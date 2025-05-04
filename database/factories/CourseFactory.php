25df9c8d8d464d5b5d82abdce85905d49dc28e1e
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2025-05-04 08:00:53 +0800

    feat: create CourseFactory

---
+++ database/factories/CourseFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Department;
+use App\Models\Instructor;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Course>
+ */
+class CourseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'department_id' => Department::inRandomOrder()->first()->departmentID,
+            'instructor_id' => Instructor::inRandomOrder()->first()->instructorID,
+            'title' => $this->faker->word,
+            'credit' => $this->faker->numberBetween(1, 4),
+        ];
+    }
+}
